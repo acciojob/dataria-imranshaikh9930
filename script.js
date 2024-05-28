23b3a71be20433bfb1720986cd877d24d5d8591f
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-05-28 13:21:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+
+function marge(arr1,arr2){
+	let n = arr1.length;
+	let m = arr2.length;
+	let tempArr= [];
+
+	let i = 0;
+	let j = 0;
+
+	while(i < n && j< m){
+
+		if(arr1[i] < arr2[j]){
+				tempArr.push(arr1[i]);
+			i++;
+		}else{
+			tempArr.push(arr2[j]);
+			j++
+		}
+		
+	}
+	while(i<n){
+	  temArr.push(arr1[i++]);
+	}
+	
+	while(j<m){
+	  tempArr.push(arr2[j++]);
+	}
+	return tempArr;
+}
+
+ console.log(marge([3, 7, 12, 20],[1, 5, 8, 18, 25]));
