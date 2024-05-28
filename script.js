25676c271bb932341fe7695ea6e066acd214db27
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-05-28 13:33:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function mergeSortedRecords(emilyRecords, oliverRecords) {
+function mergeCraftDates(emilyRecords, oliverRecords) {
     let mergedRecords = [];
     let i = 0; // pointer for Emily's records
     let j = 0; // pointer for Oliver's records
 
-    // Compare elements from both arrays and merge them
+   
     while (i < emilyRecords.length && j < oliverRecords.length) {
         if (emilyRecords[i] < oliverRecords[j]) {
             mergedRecords.push(emilyRecords[i]);
@@ -14,13 +14,13 @@ function mergeSortedRecords(emilyRecords, oliverRecords) {
         }
     }
 
-    // If there are remaining elements in Emily's records, add them
+
     while (i < emilyRecords.length) {
         mergedRecords.push(emilyRecords[i]);
         i++;
     }
 
-    // If there are remaining elements in Oliver's records, add them
+   
     while (j < oliverRecords.length) {
         mergedRecords.push(oliverRecords[j]);
         j++;
@@ -32,5 +32,5 @@ function mergeSortedRecords(emilyRecords, oliverRecords) {
 // Example usage:
 let emilyRecords = [1, 3, 5, 7];
 let oliverRecords = [2, 4, 6, 8];
-let merged = mergeSortedRecords(emilyRecords, oliverRecords);
+let merged = mergeCraftDates(emilyRecords, oliverRecords);
 console.log(merged)
