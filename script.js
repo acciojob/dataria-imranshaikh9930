925099c429d4d2570a095dabbf5d82660241c2fc
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-05-28 13:28:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,36 @@
+function mergeSortedRecords(emilyRecords, oliverRecords) {
+    let mergedRecords = [];
+    let i = 0; // pointer for Emily's records
+    let j = 0; // pointer for Oliver's records
 
-function marge(arr1,arr2){
-	let n = arr1.length;
-	let m = arr2.length;
-	let tempArr= [];
+    // Compare elements from both arrays and merge them
+    while (i < emilyRecords.length && j < oliverRecords.length) {
+        if (emilyRecords[i] < oliverRecords[j]) {
+            mergedRecords.push(emilyRecords[i]);
+            i++;
+        } else {
+            mergedRecords.push(oliverRecords[j]);
+            j++;
+        }
+    }
 
-	let i = 0;
-	let j = 0;
+    // If there are remaining elements in Emily's records, add them
+    while (i < emilyRecords.length) {
+        mergedRecords.push(emilyRecords[i]);
+        i++;
+    }
 
-	while(i < n && j< m){
+    // If there are remaining elements in Oliver's records, add them
+    while (j < oliverRecords.length) {
+        mergedRecords.push(oliverRecords[j]);
+        j++;
+    }
 
-		if(arr1[i] < arr2[j]){
-				tempArr.push(arr1[i]);
-			i++;
-		}else{
-			tempArr.push(arr2[j]);
-			j++
-		}
-		
-	}
-	while(i<n){
-	  temArr.push(arr1[i++]);
-	}
-	
-	while(j<m){
-	  tempArr.push(arr2[j++]);
-	}
-	return tempArr;
+    return mergedRecords;
 }
 
- console.log(marge([3, 7, 12, 20],[1, 5, 8, 18, 25]));
+// Example usage:
+let emilyRecords = [1, 3, 5, 7];
+let oliverRecords = [2, 4, 6, 8];
+let merged = mergeSortedRecords(emilyRecords, oliverRecords);
+console.log(merged)
